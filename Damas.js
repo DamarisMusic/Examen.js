d44c3fb7a530f3186a2870213446779c9805da75
DamarisMusic <136343381+DamarisMusic@users.noreply.github.com>
2023-09-15 08:31:19 -0300

    Add files via upload

---
+++ Damas.js
@@ -0,0 +1,26 @@
+function calcularSalarioMensual( ) 
+{
+    let salarioAnual = 21600
+    salarioAnual / 12
+    return resultadosalarioAnual
+}
+alert( )
+console.log = alert( )
+
+function calcularSalarioAnual()
+{
+    let salarioMensual 
+    salarioMensual = 1500 * 12
+    return resultadosalarioMensual
+}
+alert( )
+console.log = alert( )
+
+function calcularPromedio()
+{
+    let nota1 
+    let nota2
+    let nota3
+    calcularPromedio = nota1 + nota2 + nota3 / 3
+    return resultadoPromedio
+}
